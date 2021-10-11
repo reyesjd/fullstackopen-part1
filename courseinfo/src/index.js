d645eef503023da5a958280659aa0e3180cee774
Jesus Reyes <47608293+reyesjd@users.noreply.github.com>
2021-10-11 17:38:53 -0500

    openfullstack part 1 exercise 1.4

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -21,44 +21,47 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
-  const { part1, part2, part3 } = props;
+  const { parts } = props;
   return (
     <div>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
     </div>
   );
 };
 
 const Total = (props) => {
-  const { exercises1, exercises2, exercises3 } = props;
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+  const { parts } = props;
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
