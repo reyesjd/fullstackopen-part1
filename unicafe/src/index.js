d54ef558177c545eb524ba25f9befc88483b41df
Jesus Reyes <47608293+reyesjd@users.noreply.github.com>
2021-10-11 20:59:07 -0500

    openfullstack part 1 exercise 1.7

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -3,6 +3,47 @@ import ReactDOM from "react-dom";
 
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
+const Statics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+
+  if (all === 0) {
+    return <p>Click on any of these buttons to give your feedback</p>;
+  }
+
+  return (
+    <table>
+      <tr>
+        <th>Statistical</th>
+        <th>Value</th>
+      </tr>
+      <tr>
+        <td>good</td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>neutral</td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+      </tr>
+      <tr>
+        <td>all</td>
+        <td>{all}</td>
+      </tr>
+      <tr>
+        <td>average</td>
+        <td>{((good - bad) / all) * 100}%</td>
+      </tr>
+      <tr>
+        <td>positive</td>
+        <td>{(good / all) * 100}%</td>
+      </tr>
+    </table>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -14,9 +55,7 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
-      <h4>good {good}</h4>
-      <h4>neutral {neutral}</h4>
-      <h4>bad {bad}</h4>
+      <Statics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
